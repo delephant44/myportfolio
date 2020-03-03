ecec5fa2341353675f96e940437f5f7278ff205c
Adel Jabbar <adeljabb@gmail.com>
2020-03-03 16:03:25 -0500

    added Hobbies section w logos

--- src/components/hobbies.js
+++ src/components/hobbies.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class HobbiesComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
-        <div>
-            <h2 className="header">Hobbies & Interests</h2>
+      <div>
+        <h2 className="header">Hobbies & Interests</h2>
+        <div className="hobbyIcons">
+          <img src="/hobbyIcons/strength.png" alt="bodybuilding" />
+          <img src="/hobbyIcons/exercise.png" alt="yoga" />
+          <img src="/hobbyIcons/controller.png" alt="gaming" />
+          <img src="/hobbyIcons/guitar.png" alt="guitar" />
+          <img src="/hobbyIcons/volleyball.png" alt="volleyball" />
+          <img src="/hobbyIcons/theater.png" alt="broadway" />
+          <img src="/hobbyIcons/world.png" alt="environment" />
         </div>
+        <div className="hobbyDescription">
+          <p>
+            Outside of coding, I spend my time across a few favorite hobbies. I like to be physically active, whether it's bodybuilding, yoga, or even volleyball through the Gotham Volleyball organization. I love video games and host game nights with friends regularly. I enjoy bringing good people together! I really like Broadway shows, both plays and musicals. I'm interested in doing work to better the planet. I'm environmentally conscious and vegan.
+          </p>
+        </div>
+      </div>
     );
   }
 }
