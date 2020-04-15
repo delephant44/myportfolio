81c34fe5d87476c989775077e29ad10c1dcb28ad
Adel Jabbar <adeljabb@gmail.com>
2020-04-15 16:58:02 -0400

    ran npm build for firebase

---
+++ build/precache-manifest.8f4e04ade468f7070cb47532b6ceece1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56a91695c46998d958cd6090a046523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c04f4025a76bbad4db",
+    "url": "/static/css/main.cdc8c4ee.chunk.css"
+  },
+  {
+    "revision": "d68fac33dbb914d6fad7",
+    "url": "/static/js/2.d9dfd4c2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d9dfd4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c04f4025a76bbad4db",
+    "url": "/static/js/main.2f5de9a7.chunk.js"
+  },
+  {
+    "revision": "76ba2de154fd6435eca6",
+    "url": "/static/js/runtime-main.a20641ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
